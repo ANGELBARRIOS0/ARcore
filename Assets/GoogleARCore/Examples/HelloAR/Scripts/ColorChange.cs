b038fb8a3cfae4f0c645c7aa0eaaf4416af9e8e5
Angel Barrios <Angel.Barrios@gt.ey.com>
2020-07-16 09:46:20 -0600

    init

---
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/ColorChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChange : MonoBehaviour
+{
+    private bool colorState = false;
+    private Renderer colorCar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        colorCar = GetComponent<Renderer>();
+        colorState = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            changeColor();
+        }
+    }
+
+    private void changeColor()
+    {
+        colorCar.material.SetColor("_Color", Color.blue);
+        colorState = true;
+    }
+}
---
+++ Assets/GoogleARCore/Examples/HelloAR/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("left"))
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -1000f * Time.deltaTime));
+        }
+        if (Input.GetKey("right"))
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 1000f * Time.deltaTime));
+        }
+    }
+
+    public void left()
+    {
+        // gameObject.transform.Translate(0, 0, -50f * Time.deltaTime);
+        // gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -1000f * Time.deltaTime));
+        transform.position += Vector3.left * 1000f * Time.deltaTime;
+    }
+
+    public void right()
+    {
+        // gameObject.transform.Translate(0, 0, 50f * Time.deltaTime);
+        // gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, 1000f * Time.deltaTime));
+        transform.position += Vector3.right * 1000f * Time.deltaTime;
+    }
+}
